b10a57806ed957a260778cce598a1306f2c9d967
Mohamed Abouzaid <119829381+mo-adel-tronic@users.noreply.github.com>
2024-09-21 15:43:45 +0300

    xo project

--- lib/core/routes/pages.dart
+++ lib/core/routes/pages.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:track1_project/core/routes/routes_name.dart';
 import 'package:track1_project/pages/home_screen.dart';
 import 'package:track1_project/pages/splash_screen.dart';
+import 'package:xo_app/xo_app.dart';
 
 class AppRoute {
   static Route<dynamic> generate(RouteSettings? settings) {
@@ -13,6 +14,10 @@ class AppRoute {
       case RoutesName.home:
         return MaterialPageRoute(builder: (_) {
           return const HomeScreen();
+        });
+      case RoutesName.xoApp:
+        return MaterialPageRoute(builder: (_) {
+          return const XoApp();
         });
       default:
         throw Exception('Route not found!');
